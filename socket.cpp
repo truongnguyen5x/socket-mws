9b6bbc14f37912665f59db54b3e12456ded33d84
kairon <kairon@localhost.localdomain>
2020-04-21 16:02:54 +0700

    - Fix ssl

--- socket.cpp
+++ socket.cpp
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <cstring>
 #include <sstream>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#define FAIL    -1
+
 
 struct socks5_ident_req
 {
@@ -244,6 +248,49 @@ bool socksConnect(int fd, in_addr& dest, unsigned short port)
     }
     return true;
 }
+/*---------------------------------------------------------------------*/
+/*--- InitCTX - initialize the SSL engine.                          ---*/
+/*---------------------------------------------------------------------*/
+SSL_CTX* InitCTX(void)
+{
+
+    SSL_CTX *ctx;
+
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    const SSL_METHOD *method = TLSv1_2_client_method();
+    ctx = SSL_CTX_new(method);
+    if ( ctx == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+
+/*---------------------------------------------------------------------*/
+/*--- ShowCerts - print out the certificates.                       ---*/
+/*---------------------------------------------------------------------*/
+void ShowCerts(SSL* ssl)
+{
+    X509 *cert;
+    char *line;
+
+    cert = SSL_get_peer_certificate(ssl);    /* get the server's certificate */
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);                            /* free the malloc'ed string */
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);                            /* free the malloc'ed string */
+        X509_free(cert);                    /* free the malloc'ed certificate copy */
+    }
+    else
+        printf("No certificates.\n");
+}
 
 
 int main()
@@ -276,29 +323,45 @@ int main()
     inet_pton(AF_INET, "35.240.177.81", &(sa.sin_addr));
     if (!socksConnect(s, sa.sin_addr, 80))
         return 1;
-    std::stringstream ss;
-    ss<< "GET http://35.240.177.81/ HTTP/1.1\r\nHost: 35.240.177.81\r\n"
-      << "Connection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n"
-      << "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36\r\n"
-      << "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
-      << "Accept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n";
-    std::string str = ss.str();
-    char * query = new char[str.length()];
-    strcpy(query, str.c_str());
-    if (!sendData(s, query, strlen(query)))
-    {
-        return 0;
-    }
-    char result[4000];
-    if (!recvData(s, result, 4000))
-    {
-        return 0;
-    }
-    else
-    {
-        puts("\n");
-        puts(result);
-    }
+
+    SSL_CTX *ctx;
+    SSL *ssl;
+    ctx = InitCTX();
+    ssl = SSL_new(ctx);
+//    SSL_set_fd(ssl, s);
+//    if ( SSL_connect(ssl) == FAIL )
+//    {
+//        ERR_print_errors_fp(stderr);
+//    }
+//    else
+//    {
+        puts("ssl success");
+        std::stringstream ss;
+        ss<< "GET http://35.240.177.81/ HTTP/1.1\r\nHost: 35.240.177.81\r\n"
+          << "Connection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n"
+          << "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36\r\n"
+          << "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
+          << "Accept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n";
+        std::string str = ss.str();
+        char * query = new char[str.length()];
+        strcpy(query, str.c_str());
+        if (!sendData(s, query, strlen(query)))
+        {
+            return 0;
+        }
+        char result[4000];
+        if (!recvData(s, result, 4000))
+        {
+            return 0;
+        }
+        else
+        {
+            puts("\n");
+            puts(result);
+        }
+
+//    }
+
 
     return 0;
 }
